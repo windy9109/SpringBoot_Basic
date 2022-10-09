88fbbf86fe56fa99b02be7d0f9fc07d54962e8b8
windy9109 <windy9109@naver.com>
2022-10-09 20:13:42 +0900

    no message

---
+++ jpashop/jpashop/src/main/java/jpabook/jpashop/HelloConTroller.java
@@ -0,0 +1,17 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloConTroller {
+
+
+    @GetMapping("hello")
+    public String holle(Model model){
+       model.addAttribute("data", "hello!");
+       return "hello";
+    }
+
+}
---
+++ jpashop/jpashop/src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -0,0 +1,18 @@
+package jpabook.jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpashopApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(JpashopApplication.class, args);
+		System.out.println("안녕~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+	}
+
+
+
+}
